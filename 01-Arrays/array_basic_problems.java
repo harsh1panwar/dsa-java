64b1f458e766498543e9800f3b84584ccbce8f5c
harsh1panwar <harshpanw29@gmail.com>
2026-06-21 22:01:25 +0530

    AVERAGE OF AN ARRAY + MULTIPLY BY 10

---
+++ 01-Arrays/array_basic_problems.java
@@ -0,0 +1,36 @@
+
+import java.util.Arrays;
+public class array_basic_problems {
+//    TO GET AVERAGE OF AN ARRAY
+//        static double getAverage(int[] arr) {
+//            if (arr.length == 0) {
+//                return 0;
+//            }
+//            double sum = 0;
+//            for (int i : arr) {
+//                sum += i;
+//            }
+//            double avg = sum / arr.length;
+//            return avg;
+//        }
+//            MULTIPLY EACH ELEMENT OF ARRAY BY 10
+            static int[] multiplyBy10(int[] arr) {
+                int[] newarr = new int[arr.length];
+
+                for (int i = 0; i < arr.length; i++) {
+                    newarr[i] = arr[i] * 10;
+                }
+
+                return newarr;
+            }
+
+            public static void main(String[] args) {
+                int[] arr = {3, 1, 4, 1, 5, 9};
+
+                System.out.println(
+                        "Updated array: " +
+                                Arrays.toString(multiplyBy10(arr))
+                );
+            }
+        }
+
