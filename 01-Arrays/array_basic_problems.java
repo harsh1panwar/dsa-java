e0c768cd85e2963473237d19d973a808eafe685f
harsh1panwar <harshpanw29@gmail.com>
2026-06-22 08:07:04 +0530

    AVERAGE OF AN ARRAY + MULTIPLY BY 10+SEARCH ELEMENT

--- 01-Arrays/array_basic_problems.java
+++ 01-Arrays/array_basic_problems.java
@@ -14,23 +14,34 @@ public class array_basic_problems {
 //            return avg;
 //        }
 //            MULTIPLY EACH ELEMENT OF ARRAY BY 10
-            static int[] multiplyBy10(int[] arr) {
-                int[] newarr = new int[arr.length];
-
-                for (int i = 0; i < arr.length; i++) {
-                    newarr[i] = arr[i] * 10;
+//            static int[] multiplyBy10(int[] arr) {
+//                int[] newarr = new int[arr.length];
+//
+//                for (int i = 0; i < arr.length; i++) {
+//                    newarr[i] = arr[i] * 10;
+//                }
+//
+//                return newarr;
+//            }
+//    SEARCH FOR AN ELEMENT IN AN ARRAY
+        static int searchElement(int[] arr, int target) {
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] == target) {
+                    return i;
                 }
-
-                return newarr;
             }
-
-            public static void main(String[] args) {
-                int[] arr = {3, 1, 4, 1, 5, 9};
-
-                System.out.println(
-                        "Updated array: " +
-                                Arrays.toString(multiplyBy10(arr))
-                );
-            }
+            return -1;
         }
 
+        public static void main(String[] args) {
+            int[] arr = {3, 1, 4, 1, 5, 9};
+            int target = 4;
+            int result = searchElement(arr, target);
+
+            if (result == -1) {
+                System.out.println(target + " not found in array");
+            } else {
+                System.out.println(target + " found at index " + result);
+            }
+        }
+    }
